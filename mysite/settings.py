43c19b97d6bd6781c3fa697d9f61665854ca31c3
a <a@a-no-MacBook-Air.local>
2019-08-10 17:02:01 +0900

    done collectstatic

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,9 +123,11 @@ USE_TZ = True
 MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'media_root')
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'staticfiles', 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static_root')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+
